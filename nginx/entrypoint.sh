ffa48025e850ae83e8fea902e4b0ce2974b723a0
Minto312 <37107010xxx@gmail.com>
2023-11-20 15:13:23 +0900

    rf docker

---
+++ nginx/entrypoint.sh
@@ -0,0 +1,7 @@
+# entrypoint.sh
+
+useradd -s /bin/bash -m ${USER_NAME}
+usermod -u ${USER_ID} ${USER_NAME}
+export HOME=/home/${USER_NAME}
+groupadd -g ${GROUP_ID} ${GROUP_NAME} 
+usermod -g ${GROUP_NAME} ${USER_NAME}
