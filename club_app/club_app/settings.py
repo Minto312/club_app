0680a1843ecdea80e752a95dda0042af4a0445ae
Minto312 <karinto3219@gmail.com>
2023-11-23 22:13:56 +0000

    edit docker

--- club_app/club_app/settings.py
+++ club_app/club_app/settings.py
@@ -84,24 +84,24 @@ WSGI_APPLICATION = 'club_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.environ.get('is_docker') == 'True':    
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'club_app_db',
+            'USER': 'root',
+            'PASSWORD': 'password',
+            'HOST': 'db',
+            'PORT': '3306',
+        }
     }
-}
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'club_app_db',
-#         'USER': 'root',
-#         'PASSWORD': 'password',
-#         'HOST': 'db',
-#         'PORT': '3306',
-#     }
-# }
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
