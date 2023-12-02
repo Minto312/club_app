4a289a000bdc9429e6ea76718740d88c015c01cc
Minto312 <karinto3219@gmail.com>
2023-12-02 02:52:07 +0000

    register absent #1

--- club_app/attendance/views.py
+++ club_app/attendance/views.py
@@ -4,17 +4,8 @@ from .models import AttendanceDB, ScheduleDB
 # Create your views here.
 
 class Attendance(View):
-
     def get(self,request):
-        # すべてのレコードを表示
-        select_all = AttendanceDB.objects.all()
-
-        return_data =  {
-            'posted':'you until do not post',
-            'records':select_all
-        }
-        
-        return render(request,'attendance/attendance.html',return_data)
+        return render(request,'attendance/attendance.html')
 
     def post(self,request):
         print('==============\n\nposted run\n\n============')
@@ -53,18 +44,18 @@ def attendance_data(request, year, month):
     # 今月の出席した日付を抽出
     attended_dates = AttendanceDB.objects.filter(**extract_condition).order_by('date')
 
-    attended_days = list()
+    attended_days = dict()
     for record in attended_dates:
         date = record.date.astimezone(pytz.timezone('Asia/Tokyo'))
-        day = date.day
-        attended_days.append(day)
+        attend_condition = 'attend' if record.attended else 'absent'
+        attended_days[date.day] = attend_condition
 
     return_data = json.dumps(attended_days)
     print(f'==================\n\n/data/  {return_data=}\n\n=================')
     return HttpResponse(return_data)
 
 
-def attend(request):
+def attend(request, is_attend):
     print(f'=============\n\naccessed QR\n\n{request.user.username=} \n\n===============')
 
     username = request.user.username
@@ -73,7 +64,7 @@ def attend(request):
     insert_data =  {
         'name':username,
         'date':datetime.now(),
-        'attended':True
+        'attended': bool(is_attend)
     }
     
     if not AttendanceDB.objects.filter(
@@ -85,7 +76,7 @@ def attend(request):
         AttendanceDB.objects.create(**insert_data)
 
     # .../attendance へリダイレクト
-    return redirect('../')
+    return redirect('attendance:attendance')
 
 
 class Activity(View):
@@ -104,7 +95,6 @@ class Activity(View):
     def post(self, request):
         # 新しいレコードを作成
         date = request.POST.get('date')
-        has_club_activity = request.POST.get('has_club_activity') == 'true'
         # ボタンから日付を登録
         insert_data =  {
             'date':date,
@@ -126,13 +116,6 @@ def activity_data(request, year, month):
         scheduled_days = scheduled_dates[0].days
     else:
         scheduled_days = 'None'
-    # scheduled_days = list()
-    # for record in scheduled_date\s:
-    #     date = record.date.astimezone(pytz.timezone('Asia/Tokyo'))
-    #     day = date.day
-    #     scheduled_days.append(day)
-        
-    # return_data = json.dumps(scheduled_days)
     return_data = scheduled_days
     print(f'==================\n\n/data/  {return_data=}\n\n=================')
     return HttpResponse(return_data)
@@ -141,10 +124,8 @@ import re
 def register(request, year: int, month: int):
     print(f'=============\n\naccessed register\n\n{request.user.username=} \n\n===============')
 
-    days = request.POST.get('days')
-    print(type(days))
-    print(f'=============\n\n{days=}\n\n===============')
-    days = re.sub(r'\[|\]|\"', '', days)
+    days = request.POST['days']
+    # days = re.sub(r'\[|\]|\"', '', days)
     print(f'=============\n\n{days=}\n\n===============')
     # days = list(map(int, days.split(',')))
     # print(f'=============\n\n{days=}\n\n===============')
