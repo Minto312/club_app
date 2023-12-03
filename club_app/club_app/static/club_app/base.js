48b5352fec0f18280f01495f61bd797c062941d0
Minto312 <karinto3219@gmail.com>
2023-12-03 02:30:17 +0000

    base

--- club_app/club_app/static/club_app/base.js
+++ club_app/club_app/static/club_app/base.js
@@ -1,16 +1,41 @@
+function isSmartPhone() {
+    // UserAgentからのスマホ判定
+    if (navigator.userAgent.match(/iPhone|iPod|iPad|Android/)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+
 function showMenu(){
     console.log('showMenu');
-    $('.ready-hover').on('mouseenter',(e) => {
-        $('.ready-hover').off('mouseenter');
-        $(e.currentTarget.children[1]).addClass('showing');
-        
-        $('.ready-hover').on('mouseleave',(e) => {
-            showMenu();
-            $('.ready-hover').off('mouseleave');
-            $(e.currentTarget.children[1]).removeClass('showing');
-                console.log('done');
+    if(isSmartPhone() || true){
+        $('.ready-hover').on('click',(e) => {
+            if ($(e.currentTarget.children[1]).hasClass('showing')){
+                $(e.currentTarget.children[1]).removeClass('showing');
+            }else{
+                $('.showing').removeClass('showing');
+                $(e.currentTarget.children[1]).addClass('showing');
+                $('#content').on('click',(e) => {
+                    $('.showing').removeClass('showing');
+                    $('body').off('click');
+                });
+            }
         });
-    });
+    }else{
+        // $('.ready-hover').on('mouseenter',(e) => {
+        //     $('.ready-hover').off('mouseenter');
+        //     $(e.currentTarget.children[1]).addClass('showing');
+            
+        //     $('.ready-hover').on('mouseleave',(e) => {
+        //         showMenu();
+        //         $('.ready-hover').off('mouseleave');
+        //         $(e.currentTarget.children[1]).removeClass('showing');
+        //             console.log('done');
+        //     });
+        // });
+    }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
