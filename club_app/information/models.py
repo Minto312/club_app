0b2f41b61379473cae47b72bb082a68720144a7b
Valkyrie-takuya <yankawadev@gmail.com>
2023-12-05 12:58:35 +0900

    update

--- club_app/information/models.py
+++ club_app/information/models.py
@@ -5,9 +5,9 @@ from django.utils import timezone
 class Message_table(models.Model):
     message_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     #message
-    text_message = models.TextField()
-    image_message = models.ImageField(upload_to='message/image_message/')
-    file_message = models.FileField(upload_to='message/file_message/')
+    text_message = models.TextField(blank=True, default=None)
+    image_message = models.ImageField(upload_to='message/image_message/', default=None)
+    file_message = models.FileField(upload_to='message/file_message/', default=None)
     #send_date
     send_date = models.DateTimeField(default=timezone.now)
     
--- club_app/information/views.py
+++ club_app/information/views.py
@@ -5,30 +5,73 @@ from .models import Message_table
 class Information(View):
 
     def get(self,request):
-        message = Message_table.objects.order_by('send_date').reverse().values('file_message')
-        print(message)
-        f'''
-            <div id="message-id-{message}" class="message-wrap">
-                <div class="text-message">
-                    {message}
-                </div>
-                <div class="image_message">
-                    <img src="{message}">
-                </div>
-                <div class="file_message">
-                    <a href="{message}" download="">file name</a>
-                </div>
-            </div>
-        '''
+        messages_id = Message_table.objects.order_by('send_date').reverse().values('message_id')
+        message_context_list = []
         
+        for message_id in messages_id:
+            message = Message_table.objects.get(message_id=message_id['message_id'])
+            print(message.text_message)
+            print(message.image_message)
+            print(message.file_message)
+            message_context = [f'''<div id="message-id-{str(message_id)}" class="message-wrap">''']
 
-        return render(request,'information/information.html')
+            if message.text_message :
+                message_context.append(f'''
+                    <div class="text-message">
+                        {message.text_message}
+                    </div>
+                ''')
+            if message.image_message :
+                message_context.append(f'''
+                    <div class="image-message">
+                        <img src="/media/{message.image_message}">
+                    </div>
+                ''')
+            if message.file_message :
+                message_context.append(f'''
+                    <div class="file-message">
+                        <a href="/media/{message.file_message}" download="{message.file_message.name}">{message.file_message.name}</a>
+                    </div>
+                ''')
+            message_context_list.append(message_context)
+        context = {'messages':message_context_list}
+        return render(request,'information/information.html', context)
 
     def post(self,request):
-        return render(request,'information/information.html')
-    
 
-    #create_message
-    def create_message(self, text_message, image_message, file_message):
+        text_message = request.POST['text-message']
+        image_message = request.POST['image-message']
+        file_message = request.POST['file-message']
+
         Message_table.objects.create(text_message=text_message, image_message=image_message, file_message=file_message)
-    
+
+        messages_id = Message_table.objects.order_by('send_date').reverse().values('message_id')
+        message_context_list = []
+        for message_id in messages_id:
+            message = Message_table.objects.get(message_id=message_id['message_id'])
+            print(message.text_message)
+            print(message.image_message)
+            print(message.file_message)
+            message_context = [f'''<div id="message-id-{str(message_id)}" class="message-wrap">''']
+
+            if message.text_message :
+                message_context.append(f'''
+                    <div class="text-message">
+                        {message.text_message}
+                    </div>
+                ''')
+            if message.image_message :
+                message_context.append(f'''
+                    <div class="image-message">
+                        <img src="/media/{message.image_message}">
+                    </div>
+                ''')
+            if message.file_message :
+                message_context.append(f'''
+                    <div class="file-message">
+                        <a href="/media/{message.file_message}" download="{message.file_message.name}">{message.file_message.name}</a>
+                    </div>
+                ''')
+            message_context_list.append(message_context)
+        context = {'messages':message_context_list}
+        return render(request,'information/information.html', context)
