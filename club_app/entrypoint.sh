4d8b05e2431c177cc6c1d37a84459cb633f76766
Minto312 <37107010xxx@gmail.com>
2023-12-07 17:42:02 +0900

    nginx conf

--- club_app/entrypoint.sh
+++ club_app/entrypoint.sh
@@ -1,8 +1,8 @@
 cd /app
 
-python3 manage.py makemigrations
-python3 manage.py migrate
-python3 manage.py collectstatic
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
+python3 manage.py collectstatic --noinput
 
 python3 manage.py custom_createsuperuser --username admin --password admin
 
