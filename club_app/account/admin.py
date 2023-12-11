09de2635ee88b1bb08e2f12a326bcbcb2ee7ac18
Minto312 <37107010xxx@gmail.com>
2023-12-11 11:32:43 +0900

    rm

--- club_app/account/admin.py
+++ club_app/account/admin.py
@@ -20,7 +20,7 @@ class CustomUserAdmin(UserAdmin):
         ),
     )
 
-    list_display = ('username','password','is_staff','is_superuser')
+    list_display = ('username','is_staff','is_superuser')
 
 admin.site.register(CustomUser,CustomUserAdmin)
 admin.site.register(Profile)
